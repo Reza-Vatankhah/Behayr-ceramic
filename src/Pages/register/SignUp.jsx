5d84604d6952b9c058ee7b38d0899dc54aa0c0c6
Michael Maleki <michaelmalekibarouq@gmail.com>
2022-09-06 15:35:46 +0430

    signup form created

--- src/Pages/register/SignUp.jsx
+++ src/Pages/register/SignUp.jsx
@@ -1,195 +1,119 @@
-import React, { useEffect, useState } from "react";
-import validate from "./validate";
+import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import { notify } from "./toast";
 import "react-toastify/dist/ReactToastify.css";
 import styles from "./SignUp.module.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { useFormik } from "formik";
+import { FiUserPlus } from "react-icons/fi";
+import {BiUser} from "react-icons/bi"
+import {AiOutlineMail} from "react-icons/ai"
+import {BsPhone , BsLock} from "react-icons/bs"
 
 const SignUp = () => {
-  const [data, setData] = useState({
-    name: "",
-    family: "",
-    email: "",
-    phone: "",
-    password: "",
-    confirmPassword: "",
-    isAccepted: false,
+  // const [number, setNumber] = useState([])
+
+  // useEffect (() => {
+  //   axios.post('https://gbscoine.com/behyar/api/api/v1/register')
+  //     .then(response => setNumber(response.data))
+  // },[])
+
+  const formik = useFormik({
+    initialValues: {
+      fName: "",
+      lName: "",
+      email: "",
+      phoneNumber: "",
+      password: "",
+      confirmPassword: "",
+    },
+    onSubmit: () => {},
   });
 
-  const [errors, setErrors] = useState({});
-  const [touched, setTouch] = useState({});
-
-  useEffect(() => {
-    setErrors(validate(data, "signup"));
-    console.log(errors);
-  }, [data, touched]);
-
-  const changeHandler = (event) => {
-    if (event.target.name === "isAccepted") {
-      setData({ ...data, [event.target.name]: event.target.checked });
-    } else {
-      setData({ ...data, [event.target.name]: event.target.value });
-    }
-    console.log(data);
-  };
-
-  const focusHandler = (event) => {
-    setTouch({ ...touched, [event.target.name]: true });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    if (!Object.keys(errors).length) {
-      notify("You Signed up successfully", "success");
-    } else {
-      notify("Invalid data!", "error");
-      setTouch({
-        name: true,
-        family: true,
-        email: true,
-        phone: true,
-        password: true,
-        confirmPassword: true,
-        isAccepted: true,
-      });
-    }
-  };
-
-  const [number, setNumber] = useState([])
-
-  useEffect (() => {
-    axios.post('https://gbscoine.com/behyar/api/api/v1/register')
-      .then(response => setNumber(response.data))
-  },[])
+  const formData = [
+    {
+      name: "fName",
+      value: formik.initialValues.fName,
+      type: "text",
+      id: 0,
+      touched: formik.touched.fName,
+      errors: formik.errors.fName,
+      placeHolder: "نام",
+    },
+    {
+      name: "lName",
+      value: formik.initialValues.lName,
+      type: "text",
+      id: 1,
+      touched: formik.touched.lName,
+      errors: formik.errors.lName,
+      placeHolder: "نام خانوادگی",
+    },
+    {
+      name: "email",
+      value: formik.initialValues.email,
+      type: "email",
+      id: 2,
+      touched: formik.touched.email,
+      errors: formik.errors.email,
+      placeHolder: "ایمیل",
+    },
+    {
+      name: "phoneNumber",
+      value: formik.initialValues.phoneNumber,
+      type: "number",
+      id: 3,
+      touched: formik.touched.phoneNumber,
+      errors: formik.errors.phoneNumber,
+      placeHolder: "شماره تلفن",
+    },
+    {
+      name: "password",
+      value: formik.initialValues.password,
+      type: "password",
+      id: 4,
+      touched: formik.touched.password,
+      errors: formik.errors.password,
+      placeHolder: "گذرواژه ",
+    },
+    {
+      name: "confirmPassword",
+      value: formik.initialValues.confirmPassword,
+      type: "confirmPassword",
+      id: 5,
+      touched: formik.touched.confirmPassword,
+      errors: formik.errors.confirmPassword,
+      placeHolder: "تکرار گذرواژه ",
+    },
+  ];
 
   return (
     <div className={styles.container}>
-      <form onSubmit={submitHandler} className={styles.formContainer}>
-        <h2 className={styles.header}>SignUp</h2>
-        <div className={styles.formField}>
-          <label>Name</label>
-          <input
-            className={
-              errors.name && touched.name
-                ? styles.uncompleted
-                : styles.formInput
-            }
-            type="text"
-            name="name"
-            value={data.name}
-            onChange={changeHandler}
-            onFocus={focusHandler}
-          />
-          {errors.name && touched.name && <span>{errors.name}</span>}
+      <div className={styles.titleBox}>
+        <span>
+          <FiUserPlus />
+        </span>
+        <div className={styles.title}>
+          <h4>عضویت</h4>
+          <p>حساب کاربری خود را ایجاد کنید</p>
         </div>
-        <div className={styles.formField}>
-          <label>family</label>
-          <input
-            className={
-              errors.family && touched.family
-                ? styles.uncompleted
-                : styles.formInput
-            }
-            type="text"
-            name="family"
-            value={data.family}
-            onChange={changeHandler}
-            onFocus={focusHandler}
-          />
-          {errors.family && touched.family && <span>{errors.family}</span>}
-        </div>
-        <div className={styles.formField}>
-          <label>Email</label>
-          <input
-            className={
-              errors.email && touched.email
-                ? styles.uncompleted
-                : styles.formInput
-            }
-            type="text"
-            name="email"
-            value={data.email}
-            onChange={changeHandler}
-            onFocus={focusHandler}
-          />
-          {errors.email && touched.email && <span>{errors.email}</span>}
-        </div>
-        <div className={styles.formField}>
-          <label>Phone Number</label>
-          <input
-            className={
-              errors.phone && touched.phone
-                ? styles.uncompleted
-                : styles.formInput
-            }
-            type="number"
-            name="phone"
-            value={data.phone}
-            onChange={changeHandler}
-            onFocus={focusHandler}
-          />
-          {errors.phone && touched.phone && <span>{errors.phone}</span>}
-        </div>
-        <div className={styles.formField}>
-          <label>Password</label>
-          <input
-            className={
-              errors.password && touched.password
-                ? styles.uncompleted
-                : styles.formInput
-            }
-            type="passowrd"
-            name="password"
-            value={data.password}
-            onChange={changeHandler}
-            onFocus={focusHandler}
-          />
-          {errors.password && touched.password && (
-            <span>{errors.password}</span>
-          )}
-        </div>
-        <div className={styles.formField}>
-          <label>Confirm Password</label>
-          <input
-            className={
-              errors.confirmPassword && touched.confirmPassword
-                ? styles.uncompleted
-                : styles.formInput
-            }
-            type="password"
-            name="confirmPassword"
-            value={data.confirmPassword}
-            onChange={changeHandler}
-            onFocus={focusHandler}
-          />
-          {errors.confirmPassword && touched.confirmPassword && (
-            <span>{errors.confirmPassword}</span>
-          )}
-        </div>
-        <div className={styles.formField}>
-          <div className={styles.checkBoxContainer}>
-            <label>I accept terms of privacy policy</label>
-            <input
-              type="checkbox"
-              name="isAccepted"
-              value={data.isAccepted}
-              onChange={changeHandler}
-              onFocus={focusHandler}
-            />
-          </div>
-          {errors.isAccepted && touched.isAccepted && (
-            <span>{errors.isAccepted}</span>
-          )}
-        </div>
+      </div>
 
-        <div className={styles.formButtons}>
-          <Link to="/login">Login</Link>
-          <button type="submit">Sign Up</button>
-        </div>
+      <form>
+        {formData.map((input) => (
+          <input
+            name={input.name}
+            value={input.value}
+            key={input.id}
+            type={input.type}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            placeholder={input.placeHolder}
+          />
+        ))}
       </form>
+
       <ToastContainer />
     </div>
   );
