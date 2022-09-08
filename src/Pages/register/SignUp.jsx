7923f4cead47f0e7c4ae9aad4bdd6b95fc4cdfc0
Michael Maleki <michaelmalekibarouq@gmail.com>
2022-09-08 13:13:06 +0430

    verification completed

--- src/Pages/register/SignUp.jsx
+++ src/Pages/register/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import styles from "./SignUp.module.css";
@@ -9,18 +9,20 @@ import { AiOutlineMail } from "react-icons/ai";
 import { BsPhone, BsLock } from "react-icons/bs";
 import { BsArrowLeft } from "react-icons/bs";
 import * as Yup from "yup";
+import axios from "axios";
+import ReactInputVerificationCode from "react-input-verification-code";
 
 const SignupSchema = Yup.object().shape({
-  fName: Yup.string()
+  name: Yup.string()
     .min(3, "حداقل طول اسم باید 3 حرف باشد")
     .required("پر کردن این فیلد الزامی است"),
-  lName: Yup.string()
+  family: Yup.string()
     .min(3, "حداقل اسم باید 3 حرف باشد")
     .required("پر کردن این فیلد الزامی است"),
   email: Yup.string()
     .email("ایمیل وارد شده نامعتبر است")
     .required("پر کردن این فیلد الزامی است"),
-  phoneNumber: Yup.string()
+  phone: Yup.string()
     .required("پر کردن این فیلد الزامی است")
     .max(10, "حداکثر باید 10 رقم باشد"),
   password: Yup.string()
@@ -33,134 +35,211 @@ const SignupSchema = Yup.object().shape({
 });
 
 const SignUp = () => {
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [verificationCode, setVerificationCode] = useState(null);
+
+  console.log(verificationCode);
+  console.log(status);
+
+  const changeInfoHandler = () => {
+    setStatus(0);
+    setLoading(false)
+  };
+  const confirmHandler = () => {};
+
   return (
     <div className={styles.container}>
-      <div className={styles.titleBox}>
-        <span>
-          <FiUserPlus />
-        </span>
-        <div className={styles.title}>
-          <h4>عضویت</h4>
-          <p>حساب کاربری خود را ایجاد کنید</p>
+      {status === 200 ? (
+        <div>
+          <h1 className={styles.message}>{message} </h1>
+          <div
+            className={styles.divVerify}
+            style={{
+              direction: "ltr",
+              width: "fit-content",
+              margin: "60px auto",
+            }}
+          >
+            <ReactInputVerificationCode
+              onChange={setVerificationCode}
+              className={styles.inputvalue}
+              placeholder={""}
+            />
+          </div>
+          <div className={styles.buttonsRow}>
+            <button onClick={confirmHandler} className={styles.confirmButton}>
+              ثبت کد
+            </button>
+            <button onClick={changeInfoHandler} className={styles.changeButton}>
+              تغییر اطلاعات{" "}
+            </button>
+          </div>
         </div>
-      </div>
-      <Formik
-        initialValues={{
-          fName: "",
-          lName: "",
-          email: "",
-          phoneNumber: "",
-          password: "",
-          confirmPassword: "",
-          agree: false,
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
-        {({ errors, touched }) => (
-          <Form>
-            <div className={styles.inputsRow}>
-              <div className={styles.inputAndError}>
-                <div className={styles.inputDiv}>
-                  <span>
-                    <BiUser />
-                  </span>
-                  <Field name="fName" type="text" placeholder="نام" />
+      ) : (
+        <div>
+          <div className={styles.titleBox}>
+            <span>
+              <FiUserPlus />
+            </span>
+            <div className={styles.title}>
+              <h4>عضویت</h4>
+              <p>حساب کاربری خود را ایجاد کنید</p>
+            </div>
+          </div>
+          <Formik
+            initialValues={{
+              name: "",
+              family: "",
+              email: "",
+              phone: "",
+              password: "",
+              confirmPassword: "",
+              agree: false,
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={async (values) => {
+              const userData = {
+                name: values.name,
+                family: values.family,
+                email: values.email,
+                phone: values.phone,
+                password: values.password,
+                confirmPassword: values.confirmPassword,
+              };
+              try {
+                setLoading(true);
+                const res = await axios.post(
+                  "https://gbscoine.com/behyar/api/api/v1/register",
+                  userData
+                );
+                setStatus(res.status);
+                setMessage(res.data.message);
+                console.log(res.status);
+                console.log(res.data.message);
+                console.log(res.data.token);
+              } catch (error) {
+                console.log(error.message);
+              }
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <div className={styles.inputsRow}>
+                  <div className={styles.inputAndError}>
+                    <div className={styles.inputDiv}>
+                      <span>
+                        <BiUser />
+                      </span>
+                      <Field name="name" type="text" placeholder="نام" />
+                    </div>
+                    {errors.name && touched.name && (
+                      <span className={styles.error}>{errors.name}</span>
+                    )}
+                  </div>
+                  <div className={styles.inputAndError}>
+                    <Field
+                      name="family"
+                      type={"text"}
+                      placeholder="نام خانوادگی"
+                    />
+                    {errors.family && touched.family && (
+                      <span className={styles.error}>{errors.family}</span>
+                    )}
+                  </div>
                 </div>
-                {errors.fName && touched.fName && (
-                  <span className={styles.error}>{errors.fName}</span>
-                )}
-              </div>
-              <div className={styles.inputAndError}>
-                <Field name="lName" type={"text"} placeholder="نام خانوادگی" />
-                {errors.lName && touched.lName && (
-                  <span className={styles.error}>{errors.lName}</span>
-                )}
-              </div>
-            </div>
 
-            <div className={styles.fullInputAndError}>
-              <div className={styles.inputDivEmail}>
-                <span>
-                  <AiOutlineMail />
-                </span>
-                <Field name="email" type={"email"} placeholder="ایمیل" />
-              </div>
-              {errors.email && touched.email && (
-                <span className={styles.error}>{errors.email}</span>
-              )}
-            </div>
-
-            <div className={styles.fullInputAndError}>
-              <div className={styles.inputDivPhone}>
-                <span>
-                  <BsPhone />
-                </span>
-                <Field
-                  name="phoneNumber"
-                  type={"number"}
-                  placeholder="شماره موبایل"
-                />
-              </div>
-              {errors.phoneNumber && touched.phoneNumber && (
-                <span className={styles.error}>{errors.phoneNumber}</span>
-              )}
-            </div>
-
-            <div className={styles.inputsRow}>
-              <div className={styles.inputAndError}>
-                <div className={styles.inputDiv}>
-                  <span>
-                    <BsLock />
-                  </span>
-                  <Field
-                    name="password"
-                    type={"password"}
-                    placeholder="گذرواژه"
-                  />
+                <div className={styles.fullInputAndError}>
+                  <div className={styles.inputDivEmail}>
+                    <span>
+                      <AiOutlineMail />
+                    </span>
+                    <Field name="email" type={"email"} placeholder="ایمیل" />
+                  </div>
+                  {errors.email && touched.email && (
+                    <span className={styles.error}>{errors.email}</span>
+                  )}
                 </div>
-                {errors.password && touched.password && (
-                  <span className={styles.error}>{errors.password}</span>
-                )}
-              </div>
-              <div className={styles.inputAndError}>
-                <Field
-                  name="confirmPassword"
-                  type={"password"}
-                  placeholder="تکرار گذرواژه"
-                />
-                {errors.confirmPassword && touched.confirmPassword && (
-                  <span className={styles.error}>{errors.confirmPassword}</span>
-                )}
-              </div>
-            </div>
-            <div className={styles.agreeDiv}>
-              <label>
-                <Field name="agree" type="checkbox" id="agree" />
-                با قوانین و مقررات سایت موافقم
-              </label>
-              {errors.agree && touched.agree && (
-                <span className={styles.error}>{errors.agree}</span>
-              )}
-            </div>
-            <div className={styles.buttonsRow}>
-              <button type="submit" className={styles.signupButton}>
-                <p>ثبت نام</p>
-                <span>
-                  <BsArrowLeft />
-                </span>
-              </button>
-              <div className={styles.loginDiv}>
-                <span>حساب کاربری دارید؟</span>
-                <button className={styles.loginButton}>وارد شوید</button>
-              </div>
-            </div>
-          </Form>
-        )}
-      </Formik>
-      <ToastContainer />
+
+                <div className={styles.fullInputAndError}>
+                  <div className={styles.inputDivPhone}>
+                    <span>
+                      <BsPhone />
+                    </span>
+                    <Field
+                      name="phone"
+                      type={"number"}
+                      placeholder="شماره موبایل"
+                    />
+                  </div>
+                  {errors.phone && touched.phone && (
+                    <span className={styles.error}>{errors.phone}</span>
+                  )}
+                </div>
+
+                <div className={styles.inputsRow}>
+                  <div className={styles.inputAndError}>
+                    <div className={styles.inputDiv}>
+                      <span>
+                        <BsLock />
+                      </span>
+                      <Field
+                        name="password"
+                        type={"password"}
+                        placeholder="گذرواژه"
+                      />
+                    </div>
+                    {errors.password && touched.password && (
+                      <span className={styles.error}>{errors.password}</span>
+                    )}
+                  </div>
+                  <div className={styles.inputAndError}>
+                    <Field
+                      name="confirmPassword"
+                      type={"password"}
+                      placeholder="تکرار گذرواژه"
+                    />
+                    {errors.confirmPassword && touched.confirmPassword && (
+                      <span className={styles.error}>
+                        {errors.confirmPassword}
+                      </span>
+                    )}
+                  </div>
+                </div>
+                <div className={styles.agreeDiv}>
+                  <label>
+                    <Field name="agree" type="checkbox" id="agree" />
+                    با قوانین و مقررات سایت موافقم
+                  </label>
+                  {errors.agree && touched.agree && (
+                    <span className={styles.error}>{errors.agree}</span>
+                  )}
+                </div>
+                <div className={styles.buttonsRow}>
+                  <button
+                    type="submit"
+                    className={`${styles.signupButton} ${
+                      loading && styles.loading
+                    }`}
+                    disabled={loading}
+                  >
+                    <p>ثبت نام</p>
+                    <span>
+                      <BsArrowLeft />
+                    </span>
+                  </button>
+                  <div className={styles.loginDiv}>
+                    <span>حساب کاربری دارید؟</span>
+                    <button className={styles.loginButton}>وارد شوید</button>
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
+          <ToastContainer />
+        </div>
+      )}
     </div>
   );
 };
